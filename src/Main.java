9afee24c469bddfb8fb4e3420575892defe7a2e4
artem bystrov <ockidocki1@gmail.com>
2023-03-10 16:15:09 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int amount = 100;
+        int topup = 1100;
+
+        if (topup >= 1000)
+            System.out.println(amount + topup + topup / 100);
+
+        if (topup < 1000)
+            System.out.println(amount + topup);
+
+    }
+}
